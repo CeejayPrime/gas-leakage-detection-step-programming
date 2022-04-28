e7856534f3f9eaf7db1fb9ab15f8d5a502d75faf
CeejayPrime <36557279+CeejayPrime@users.noreply.github.com>
2022-04-28 14:12:34 +0100

    Create steps.cpp

---
+++ steps.cpp
@@ -0,0 +1,47 @@
+
+
+#define SERIAL_DEBUG_BAUD                 9600
+
+#define GAS_SENSOR_1                      A0
+//#define GAS_SENSOR_2                      A2
+//#define GAS_SENSOR_3                      A6
+
+#define BUZZER_ALARM                       4
+
+//#define LED_RED
+
+int VAL_GAS_1 = 0;
+//int VAL_GAS_2 = 0;
+//int VAL_GAS_3 = 0;
+
+
+//RTC_DS3231 rtc;
+
+unsigned long lastSend;
+unsigned long endMillis;
+unsigned long startMillis;
+
+void setup () {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  VAL_GAS_1 = analogRead(GAS_SENSOR_1);
+  //  VAL_GAS_2 = analogRead(GAS_SENSOR_2);
+  //  VAL_GAS_3 = analogRead(GAS_SENSOR_3);
+
+  //  Serial.println("Gas 2 sensor reading" + String(VAL_GAS_2));
+  //  Serial.println("Gas 3 sensor reading" + String(VAL_GAS_3));
+
+
+  Serial.println("Sending data...");
+
+  endMillis = millis();
+  unsigned long sendTelemetryTime = endMillis - startMillis;
+
+  if (millis() - lastSend > 1000) {
+    Serial.println("Gas 1 sensor reading" + String(VAL_GAS_1));
+    lastSend = millis();
+  }
+}
